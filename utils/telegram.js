0e190b25c54253aa20087c6f56182a9b4272a8e0
viram370 <gamringhackking@gmail.com>
2026-07-06 07:48:00 +0530

    Implement Telegram message retrieval and video check
    Added functions to retrieve messages and check for video media in Telegram.

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,1 +1,31 @@
 
+const { Api } = require("telegram");
+
+/**
+ * Get a message from a Telegram channel.
+ */
+async function getMessage(client, channelId, messageId) {
+    const messages = await client.getMessages(channelId, {
+        ids: [Number(messageId)]
+    });
+
+    return messages[0] || null;
+}
+
+/**
+ * Check if message contains a video.
+ */
+function getVideoMedia(message) {
+    if (!message || !message.media) return null;
+
+    if (message.media instanceof Api.MessageMediaDocument) {
+        return message.media.document;
+    }
+
+    return null;
+}
+
+module.exports = {
+    getMessage,
+    getVideoMedia
+};
